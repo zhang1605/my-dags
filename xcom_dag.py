e5cfe8c0b8615d75d976911e08537c563259b0d4
Michael Zhang <zhang1605@gmail.com>
2022-04-12 10:57:13 -0700

    xcom_dag

---
+++ xcom_dag.py
@@ -0,0 +1,39 @@
+# dags/xcom_dag.py
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from random import uniform
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1)
+}
+
+def _training_model():
+    accuracy = uniform(0.1, 10.0)
+    print(f'model\'s accuracy: {accuracy}')
+
+def _choose_best_model():
+    print('choose best model')
+
+with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+
+    downloading_data = BashOperator(
+        task_id='downloading_data',
+        bash_command='sleep 3'
+    )
+
+    training_model_task = [
+        PythonOperator(
+            task_id=f'training_model_{task}',
+            python_callable=_training_model
+        ) for task in ['A', 'B', 'C']]
+
+    choose_model = PythonOperator(
+        task_id='choose_model',
+        python_callable=_choose_best_model
+    )
+
+    downloading_data >> training_model_task >> choose_model
