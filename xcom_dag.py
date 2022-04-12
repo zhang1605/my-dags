26fbe80819d2da73e57a50c0dd14c019a10cae0e
Michael Zhang <zhang1605@gmail.com>
2022-04-12 11:19:46 -0700

    test xcom

--- xcom_dag.py
+++ xcom_dag.py
@@ -1,7 +1,6 @@
 # dags/xcom_dag.py
 
 from airflow import DAG
-from airflow.decorators import task 
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
@@ -12,44 +11,23 @@ default_args = {
     'start_date': datetime(2020, 1, 1)
 }
 
-def _training_model():
+def _training_model(ti):
     accuracy = uniform(0.1, 10.0)
     print(f'model\'s accuracy: {accuracy}')
+    ti.xcom_push(key='model_accuracy', value=accuracy)
 
-def _choose_best_model():
+def _choose_best_model(ti):
     print('choose best model')
+    accuracies = ti.xcom_pull(key='model_accuracy', task_ids=['training_model_A', 'training_model_B', 'training_model_C'])
+    print(accuracies)
 
 with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
     downloading_data = BashOperator(
         task_id='downloading_data',
-        bash_command='sleep 3'
+        bash_command='sleep 3',
+        do_xcom_push=False
     )
-
-    @task.virtualenv(
-        task_id="virtualenv_python", requirements=["colorama==0.4.0"], system_site_packages=False
-    )
-    def callable_virtualenv():
-        """
-        Example function that will be performed in a virtual environment.
-
-        Importing at the module level ensures that it will not attempt to import the
-        library before it is installed.
-        """
-        from time import sleep
-
-        from colorama import Back, Fore, Style
-
-        print(Fore.RED + 'some red text')
-        print(Back.GREEN + 'and with a green background')
-        print(Style.DIM + 'and in dim text')
-        print(Style.RESET_ALL)
-        for _ in range(10):
-            print(Style.DIM + 'Please wait...', flush=True)
-            sleep(10)
-        print('Finished')
-
-    virtualenv_task = callable_virtualenv()
 
     training_model_task = [
         PythonOperator(
@@ -62,4 +40,4 @@ with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catc
         python_callable=_choose_best_model
     )
 
-    downloading_data >> virtualenv_task >>  training_model_task >> choose_model
+    downloading_data >> training_model_task >> choose_model
